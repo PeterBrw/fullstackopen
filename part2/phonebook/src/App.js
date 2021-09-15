77e505cd7977673e89636fd9da72468392b7a911
PeterBrw <peter.giurgiu@gmail.com>
2021-09-15 20:23:34 +0300

    part2 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,9 +4,15 @@ import Form from './components/Form'
 import Number from './components/Number'
 
 const App = () => {
-	const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-123456' }])
+	const [persons, setPersons] = useState([
+		{ name: 'Arto Hellas', number: '040-123456', id: 1 },
+		{ name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+		{ name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+		{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+	])
 	const [newName, setNewName] = useState('add a new name')
 	const [newNumber, setNewNumber] = useState('add the number')
+	const [filter, setFilter] = useState('')
 
 	const addName = (e) => {
 		e.preventDefault()
@@ -33,7 +39,7 @@ const App = () => {
 			return
 		}
 
-		newPersons.push({ name: newName, number: newNumber })
+		newPersons.push({ name: newName, number: newNumber, id: new Date() })
 		setPersons(newPersons)
 		setNewName('')
 		setNewNumber('')
@@ -41,10 +47,15 @@ const App = () => {
 
 	const handleNameChange = (e) => setNewName(e.target.value)
 	const handleNumberChange = (e) => setNewNumber(e.target.value)
+	const handleFilterChange = (e) => setFilter(e.target.value)
 
 	return (
 		<div>
 			<Header text={'Phonebook'} />
+			<div>
+				filter shown with: <input onChange={handleFilterChange} />
+			</div>
+			<Header text={'add a new'} />
 			<Form
 				addName={addName}
 				newName={newName}
@@ -55,9 +66,11 @@ const App = () => {
 				setNewNumber={setNewNumber}
 			/>
 			<Header text={'Numbers'} />
-			{persons.map((person, i) => (
-				<Number key={person.name} name={person.name} number={person.number} />
-			))}
+			{persons
+				.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()))
+				?.map((person, i) => (
+					<Number key={person.id} name={person.name} number={person.number} />
+				))}
 		</div>
 	)
 }
