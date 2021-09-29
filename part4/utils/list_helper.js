bf2268f54dbc1efa1a8f31e45e2b953609896178
PeterBrw <peter.giurgiu@gmail.com>
2021-09-30 01:18:27 +0300

    part4 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,4 +2,8 @@ const dummy = (blogs) => {
     return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+    return blogs.reduce((accu, curr) => accu + curr.likes, 0) || 0
+}
+
+module.exports = { dummy, totalLikes }
